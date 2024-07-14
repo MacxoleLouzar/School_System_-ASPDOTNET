9c5aae87363ede9944a152beca4467ee012cef83
Masxole Loza <macxolelouzaar@gmail.com>
2024-07-14 19:50:27 +0200

    Grade Interface

---
+++ api/Interfaces/IGradeRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Model;
+
+namespace api.Interfaces
+{
+    public interface IGradeRepository
+    {
+        Task<List<Grades>> GetGradesAsync();
+        Task<Grades> GetGradeByIdAsync(int gradeId);
+        Task<Grades> CreateGradeAsync(Grades grade);
+        Task<Grades> UpdateGradeAsync(int id, Grades grade);
+        Task<Grades> DeleteGradeAsync(int gradeId);
+    }
+}
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+855812961ef8aeef84c3f594acfed8539d6f0500")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5c7620bd852cd1307a806297494f79e44d2c298")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
